0d1156596a9622fa836e86e1d0d2e159ad744bfd
Lucas Ron <ronlucas86@gmail.com>
2021-01-17 22:11:24 -0300

    first commit

---
+++ src/main/java/com/ronlucas/testContainersDemo/TestContainersDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ronlucas.testContainersDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainersDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainersDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronlucas/testContainersDemo/TestContainersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronlucas.testContainersDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContainersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
